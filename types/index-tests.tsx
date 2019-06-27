d2cbef7b37251e96c1b65d0f80a90b7660c2bcac
Keisuke Kan <9renpoto@gmail.com>
2019-06-27 13:05:04 +0900

    fix(types) run tests

---
+++ types/index-tests.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Ticker from '.'
+
+const MoveStuffAround = () => (
+    <Ticker>
+        <h1>This is the Headline of element!</h1>
+        <img src="www.my-image-source.com/" alt=""/>
+    </Ticker>
+)
+
+export default MoveStuffAround
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,11 +3,10 @@ import * as React from 'react'
 type Props = {
   direction?: string,
   mode?: string,
-  move?: bool,
-  offset?: Array<number> | Array<string>,
+  move?: boolean,
+  offset?: number | string,
   speed?: number,
-  height?: Array<number> | Array<string>
+  height?: number | string
 }
 
-export default class Ticker extends React.Component<Props> {
-}
+export default class Ticker extends React.Component<Props> {}
