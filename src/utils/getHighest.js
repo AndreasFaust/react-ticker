e5b813f23a99fdc16f86f5a624eda0d3ce367323
Cody Crandall <cody@halfhelix.com>
2021-11-24 15:27:10 -0600

    check if rect exists before attempting to get height prop

--- src/utils/getHighest.js
+++ src/utils/getHighest.js
@@ -1,7 +1,7 @@
 export default function getHighest(elements) {
   let highest = 0
   elements.forEach(el => {
-    if (el.rect.height > highest) {
+    if (el.rect && el.rect.height > highest) {
       highest = el.rect.height
     }
   })
